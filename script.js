8ca9583d6d80d815ae8dea480afecd19038d029d
iansaf <iansaf@ga.github.io>
2021-10-11 09:23:33 +0800

    q

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-var time = new Date().getDate()
+var time = new Date().getDate();
+var d = new Date().toLocaleString('ja-JP-u-ca-chinese');
+var nowdate = get(d);
+
+function get (dd) {
+    if (dd.substring(6,7) == '日') {
+        return dd.substring(5,6);
+    }else{
+        return dd.substring(5,7);
+    };
+};
+
+//nowdate.substring(4,5);
+
 
 console.log(time);
+console.log(nowdate);
